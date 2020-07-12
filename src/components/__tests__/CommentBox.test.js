18684862490db826e132b3ab66c9dc85de588990
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-07-12 12:37:06 +0300

    AfterEach Statements

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -3,8 +3,18 @@ import { mount } from 'enzyme'
 
 import CommentBox from '~/CommentBox'
 
+let wrapped
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />)
+})
+
+afterEach(() => {
+  wrapped.unmount()
+})
+
 it('has a text area and a button', () => {
-  const wrapped = mount(<CommentBox />)
+  wrapped = mount(<CommentBox />)
 
   expect(wrapped.find('textarea').length).toEqual(1)
   expect(wrapped.find('button').length).toEqual(1)
