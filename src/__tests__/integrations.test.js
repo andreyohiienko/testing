bdc5e693c60a3c3e4deaf316381469bc5729a2c7
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-07-14 01:45:40 +0300

    Fixing a broken test

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -5,6 +5,11 @@ import App from '~/App'
 
 it('can fetch a list of comments and display them', () => {
   // Attempt to render the *entire* app
+  const wrapped = mount(
+    <Root>
+      <App />
+    </Root>,
+  )
   // find the 'fetchComments' button click it
   // Expect to find a list of comments!
 })
