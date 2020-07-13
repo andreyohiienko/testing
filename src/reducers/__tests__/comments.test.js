57e10dd9332b910dea464fc512d0d0519cff8a13
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-07-13 17:39:28 +0300

    Handling unknown types

--- src/reducers/__tests__/comments.test.js
+++ src/reducers/__tests__/comments.test.js
@@ -10,3 +10,8 @@ it('handles actions of type SAVE_COMMENT', () => {
   const newState = commentsReducer([], action)
   expect(newState).toEqual(['New Comment'])
 })
+
+it('handles action with unknown type', () => {
+  const newState = commentsReducer([], { type: 'jk;ljk;' })
+  expect(newState).toEqual([])
+})
