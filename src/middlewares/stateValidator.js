4a7380cedff5104977fc35c75c0e5e7fdf247ccb
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-07-14 18:01:36 +0300

    Emitting Warnings

--- src/middlewares/stateValidator.js
+++ src/middlewares/stateValidator.js
@@ -4,5 +4,7 @@ import stateSchema from './stateSchema'
 export default ({ dispatch, getState }) => (next) => (action) => {
   next(action)
 
-  console.log(tv4.validate(getState(), stateSchema))
+  if (!tv4.validate(getState(), stateSchema)) {
+    console.warn('Inwalid state schema detected')
+  }
 }
