e13f89aaf46264dee46f10fbdf4244b26f8bcc2a
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-07-14 12:57:38 +0300

    Auth Reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { SAVE_COMMENT, FETCH_COMMENTS } from './types'
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from './types'
 import axios from 'axios'
 
 export const saveComment = (comment) => ({
@@ -12,5 +12,12 @@ export function fetchComments() {
   return {
     type: FETCH_COMMENTS,
     payload: response,
+  }
+}
+
+export function changeAuth(isLoggedIn) {
+  return {
+    type: CHANGE_AUTH,
+    paylod: isLoggedIn,
   }
 }
