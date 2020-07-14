68a6a63aae7eac0cbf989c2eb96ec1468b4e43fb
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-07-14 16:44:02 +0300

    Forwarding actions

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,1 +1,11 @@
-export default ({ dispatch }) => (next) => (action) => {}
+export default ({ dispatch }) => (next) => (action) => {
+  // Check to see if the action
+  // has a promise on its 'payload' property
+  // It it does, then wait for it to resolve
+  // If it doesn't, then send the action on to the
+  // next middleware
+
+  if (!action.payload || !action.payload.then) {
+    return next(action)
+  }
+}
